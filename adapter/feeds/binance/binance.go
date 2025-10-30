89736c161a12110c630ab2b7d9e7ad964fd7be3b
Aditya Kumar <adikumar306@gmail.com>
2025-10-31 00:01:58 +0530

    error handling in binance subscribe

--- adapter/feeds/binance/binance.go
+++ adapter/feeds/binance/binance.go
@@ -36,10 +36,36 @@ func (b *BinanceSubscriber) Subscribe(conn *websocket.Conn) error {
 		return logger.LogAndWrap("Error creating subscribe message for binance", err, "feed", "binance")
 	}
 
-	err = conn.WriteJSON(subscribeJson)
+	err = conn.WriteMessage(websocket.TextMessage, subscribeJson)
 	if err != nil {
-		return logger.LogAndWrap("Error subscribing to binance", err, "feed", "binance")
+		return logger.LogAndWrap("Error writing subscribe message to binance connection", err, "feed", "binance", "stream", b.Channel)
 	}
+
+	_, msg, readErr := conn.ReadMessage()
+	if readErr != nil {
+		return logger.LogAndWrap("Error reading subscribe acknowledgement in binance connection", err, "feed", "binance", "stream", b.Channel)
+	}
+
+	// resp can be {"result": null, "id": 1}
+	// or
+	// {"code": 400, "msg": "Bad request"} - error case
+	var okResp struct {
+		Result interface{} `json:"result"`
+		Id     int         `json:"id"`
+		Code   int         `json:"code"`
+		Msg    string      `json:"msg"`
+	}
+
+	if err = json.Unmarshal(msg, &okResp); err != nil {
+		return logger.LogAndWrap("Error in parsing subscribe response for Binance", err, "feed_name", "binance", "stream", b.Channel)
+	}
+
+	// ERROR CASE
+	if okResp.Code != 0 {
+		return logger.LogAndWrap("Got error response from Binance upon subscription", nil, "code", okResp.Code, "msg", okResp.Msg)
+	}
+
+	logger.Log.Info("Successfully subscribed to Binance stream", "stream_channel", b.Channel)
 
 	return nil
 }
